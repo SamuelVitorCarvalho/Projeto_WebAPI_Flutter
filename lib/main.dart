66d2200b9489852ef2e06b479a9a9bba4ae98baf
Samuel-Carvalho5674 <samuelvitor124@gmail.com>
2024-10-16 20:44:40 -0300

    Alteração do tema da appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/home_screen/home_screen.dart';
 
 void main() {
@@ -7,11 +8,22 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Simple Journal',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.black,
+            elevation: 5,
+            shadowColor: Colors.grey,
+            titleTextStyle: TextStyle(color: Colors.white, fontSize: 18),
+          ),
+          textTheme: GoogleFonts.bitterTextTheme()
+      ),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
       initialRoute: "home",
